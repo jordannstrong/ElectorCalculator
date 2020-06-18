89988eeab7ebb52635e5fd8b526239cf057cd5e2
jordannstrong <jordannstrong@gmail.com>
2020-06-18 01:40:22 -0400

    Prep for deploy to Google Cloud

--- src/app/polls.service.ts
+++ src/app/polls.service.ts
@@ -10,7 +10,7 @@ export class PollsService {
     }
 
     getData(): Observable<any> {        
-        return this.httpClient.get('http://127.0.0.1:5002/polls');
+        return this.httpClient.get('https://evmapserver.ue.r.appspot.com/polls');
     }
 
 }
